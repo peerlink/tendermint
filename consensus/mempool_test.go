3f022619f376393f595fa7e71421649b6eeb723c
Anton Kaliaev <anton.kalyaev@gmail.com>
2018-08-08 16:03:58 +0400

    rename MaxBytes to MaxTxsBytes
    allow to set filter on the mempool to filter transactions
    
    Refs #2035

--- consensus/mempool_test.go
+++ consensus/mempool_test.go
@@ -149,7 +149,7 @@ func TestMempoolRmBadTx(t *testing.T) {
 
 		// check for the tx
 		for {
-			txs := cs.mempool.Reap(1)
+			txs := cs.mempool.Reap(1, -1)
 			if len(txs) == 0 {
 				emptyMempoolCh <- struct{}{}
 				return
